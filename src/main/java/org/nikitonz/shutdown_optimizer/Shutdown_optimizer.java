38c34b064fd0e82f4ad7bd21d87616bc54beaa70
Nikitonz <100299461+Nikitonz@users.noreply.github.com>
2023-05-20 15:10:55 +0300

    created

---
+++ src/main/java/org/nikitonz/shutdown_optimizer/Shutdown_optimizer.java
@@ -0,0 +1,17 @@
+package org.nikitonz.shutdown_optimizer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Shutdown_optimizer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
